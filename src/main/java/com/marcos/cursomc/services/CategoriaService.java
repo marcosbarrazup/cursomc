b4fd96abe6a2e8fa531181010f6130dc948bf0b9
Marcos Barra <marcos.barra@zup.com.br>
2018-09-29 15:09:33 -0300

    Criando repository e serice para Categoria

---
+++ src/main/java/com/marcos/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.marcos.cursomc.services;
+
+import com.marcos.cursomc.domain.Categoria;
+import com.marcos.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
