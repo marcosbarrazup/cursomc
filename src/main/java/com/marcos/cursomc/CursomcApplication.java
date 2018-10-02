5b196132ea862acfef626b9b83c6224cbd9f5360
Marcos Barra <marcos.barra@zup.com.br>
2018-10-01 23:53:33 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/marcos/cursomc/CursomcApplication.java
+++ src/main/java/com/marcos/cursomc/CursomcApplication.java
@@ -1,6 +1,7 @@
 package com.marcos.cursomc;
 
 import com.marcos.cursomc.domain.*;
+import com.marcos.cursomc.enums.EstadoPagamento;
 import com.marcos.cursomc.enums.TipoCliente;
 import com.marcos.cursomc.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -25,6 +27,10 @@ public class CursomcApplication implements CommandLineRunner {
     private ClienteRepository clienteRepository;
     @Autowired
     private EnderecoRepository enderecoRepository;
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 
@@ -53,6 +59,17 @@ public class CursomcApplication implements CommandLineRunner {
         Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
         Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+        Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+        Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+        Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+        ped1.setPagamento(pagto1);
+        Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+        ped2.setPagamento(pagto2);
+
+        cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 
         cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
         cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
@@ -73,6 +90,8 @@ public class CursomcApplication implements CommandLineRunner {
         cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
         clienteRepository.saveAll(Arrays.asList(cli1));
         enderecoRepository.saveAll(Arrays.asList(e1, e2));
+        pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+        pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 
 
     }
